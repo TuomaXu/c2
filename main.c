5b222ef52f33b0c0f0f34aa5014aac1f90de1659
tuomaxu <you@example.com>
2019-01-15 14:23:57 +0800

    构建应用功能框架

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("敏感字符管理系统\n");
+
+    while (1)
+    {
+        printf("1---添加敏感字符\n");
+        printf("2---删除敏感字符（最后一个）\n");
+        printf("3---查看所有敏感字符\n");
+        printf("4---替换内容中的敏感字符\n");
+        printf("5---退出\n");
+
+        printf("请选择相应功能\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            printf("退出系统\n");
+            break;
+        }
+
+
+
+    }
 
     return 0;
-
 }
